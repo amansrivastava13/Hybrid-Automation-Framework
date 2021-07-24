96d7e3e22984d5479edb16b8f29178f54c2ce266
amansrivastava13 <aman.srivastava1@delhivery.com>
2021-07-24 15:42:20 +0530

    Git Deployment

---
+++ inetBankingV1/src/test/java/com/inetbanking/testCases/TC_AddCustomerTest_003.java
@@ -0,0 +1,65 @@
+package com.inetbanking.testCases;
+
+import java.io.IOException;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.inetbanking.pageObjects.AddCustomerPage;
+import com.inetbanking.pageObjects.LoginPage;
+
+public class TC_AddCustomerTest_003 extends BaseClass
+{
+	@Test
+	public void addNewCustomer() throws InterruptedException, IOException
+	{
+		LoginPage lp = new LoginPage(driver);
+		lp.setUserName(username);
+		logger.info("username is provided");
+		lp.setPassword(password);
+		logger.info("password is provided");
+		lp.clickSubmit();
+		
+		Thread.sleep(3000);
+		
+		AddCustomerPage addcust = new AddCustomerPage(driver);
+		addcust.clickAddNewCustomer();
+		
+		logger.info("providing customer details...");
+		
+		addcust.custName("Warrior");
+		addcust.custgender("male");
+		addcust.custdob("10", "13", "1997");
+		Thread.sleep(3000);
+		addcust.custaddress("India");
+		addcust.custcity("Lucknow");
+		addcust.custstate("UP");
+		addcust.custpinno("226027");
+		addcust.custtelephoneno("987901456");
+		// Now we have to generate email id dynamically. Because this system will not take the same-id again
+		String email = randomString()+"@gmail.com";
+		addcust.custemailid(email);
+		addcust.custpassword("qwerty12345");
+		addcust.custsubmit();
+		
+		Thread.sleep(3000);
+		
+		logger.info("validation started...");
+		
+		boolean res = driver.getPageSource().contains("Customer Registered Successfully!!!");
+		
+		if(res == true)
+		{
+			Assert.assertTrue(true);
+			logger.info("test case passed...");
+		}
+		else
+		{
+			logger.info("test case failed...");
+			captureScreen(driver,"addNewCustomer");
+			Assert.assertTrue(false);
+		}
+	}
+		
+}
